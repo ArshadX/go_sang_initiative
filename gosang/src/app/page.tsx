f516e786775691af3c15b321da9e4ebf56c491c6
Vimal Sharma <vimalsharma0753@gmail.com>
2024-09-07 14:51:30 +0530

    slider added

--- gosang/src/app/page.tsx
+++ gosang/src/app/page.tsx
@@ -4,12 +4,14 @@ import Footer from "@/components/footer/Footer";
 import SelectService from "@/components/home/SelectService";
 import ChooseLocation from "@/components/home/ChooseLocation";
 import RideCard from "@/components/rides/RideCard";
+import PromotionSlider from "@/components/home/PromotionSlider";
 
 export default function Home() {
   return (
     <>
     <SelectService/>
     <ChooseLocation/>
+    <PromotionSlider/>
     </>
   );
 }
