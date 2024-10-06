815fde13000f4f48f51b1111b6513f88af5916d3
ArshadX <mas12542@outlook.com>
2024-10-06 18:57:57 +0530

    cookies setup test1

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -7,8 +7,8 @@ import {
 } from '@headlessui/react';
 import Image from 'next/image';
 import { RiArrowDownSLine, RiChat4Line, RiCustomerServiceLine, RiMoneyRupeeCircleLine, RiPhoneLine, RiShieldCheckLine, RiTeamLine } from 'react-icons/ri';
-import { verifySession } from '@/lib/dal';
 import { Logout } from '@/widgets/Logout';
+import { getCredentials } from '@/app/actions/auth';
 
 const about = [
   { name: 'Who we are', description: '', href: '/about', icon: RiTeamLine },
@@ -23,8 +23,8 @@ const callsToAction = [
 ];
 
 export default async function Header() {
-  const session = await verifySession()
-  console.log(session);
+  const session = await getCredentials()
+  console.log("credentials in header",session);
 
   // Optionally handle the case where isLoggedIn is undefined (e.g., during the hook's execution)
   return (
@@ -99,6 +99,13 @@ export default async function Header() {
           </Popover>
         </PopoverGroup>
         <div className="flex lg:flex-1 lg:justify-end">
+             <a
+              href="/register"
+              className="inline-flex items-center gap-2 rounded-md bg-primary py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none hover:bg-pink-600 open:bg-pink-600 data-[focus]:outline-1 data-[focus]:outline-white"
+            >
+              {`${session.isAuth},${session.token}`}
+              {/* <span aria-hidden="true">&rarr;</span> */}
+            </a>
           {!session?.isAuth ? (
             <div className='space-x-6'>
             <a
